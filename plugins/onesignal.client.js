ce331027e93ba557ef4ad58fc5f75d38d4a83ecd
Magical Calcifer <magicalcifer@protonmail.com>
2024-01-23 21:14:12 +0800

    ??

--- plugins/onesignal.client.js
+++ plugins/onesignal.client.js
@@ -8,7 +8,7 @@ export default defineNuxtPlugin((nuxtApp) => {
 //     // safari_web_id: "web.onesignal.auto.27be598e-7a22-4ed6-a01a-10378439b214",
 //   })
 
-const OneSignal = useOneSignal();
+// const OneSignal = useOneSignal();
 
   // OneSignal.init({
   //   appId:'128191f3-21e0-40b9-9567-4d7c0cc0e387',
