a785bde5de3160852e06754ae8b0ab1e381fc129
Magical Calcifer <magicalcifer@protonmail.com>
2024-01-23 21:28:30 +0800

    ???

--- app.vue
+++ app.vue
@@ -73,8 +73,10 @@ const requestConnect = async () => {
 }
 
 const { $pwa, $OneSignal } = useNuxtApp()
+// console.log(useNuxtApp())
 
 const optInNotification = async () => {
+  console.log($OneSignal)
   let result = await $OneSignal.User.PushSubscription.optIn();
   console.log(result)
 }
--- plugins/onesignal.client.js
+++ plugins/onesignal.client.js
@@ -1,6 +1,7 @@
 
-import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
+// import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 // import { useOneSignal } from '@onesignal/onesignal-vue3';
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 
 export default defineNuxtPlugin((nuxtApp) => {
 //   nuxtApp.vueApp.use(OneSignalVuePlugin, {
@@ -15,9 +16,31 @@ export default defineNuxtPlugin((nuxtApp) => {
   //   allowLocalhostAsSecureOrigin: process.env.APP_ENV !== 'production',
   // });
 
+  // const OneSignal = useOneSignal();
+
+  // OneSignal.init({
+  //   appId:'128191f3-21e0-40b9-9567-4d7c0cc0e387',
+  //   allowLocalhostAsSecureOrigin: process.env.APP_ENV !== 'production',
+  // });
+
+  // console.log(OneSignal)
+
   nuxtApp.vueApp.use(OneSignalVuePlugin, {
-    appId: '128191f3-21e0-40b9-9567-4d7c0cc0e387'
-  });
+    appId: "128191f3-21e0-40b9-9567-4d7c0cc0e387",
+    safari_web_id: "web.onesignal.auto.27be598e-7a22-4ed6-a01a-10378439b214",
+    notifyButton: {
+        enable: true,
+    },
+    allowLocalhostAsSecureOrigin: true,
+})
+
+  // nuxtApp.vueApp.use(OneSignal)
+  // nuxtApp.vueApp.use(OneSignalVuePlugin, {
+  //   appId: '128191f3-21e0-40b9-9567-4d7c0cc0e387',
+  //   allowLocalhostAsSecureOrigin: true
+  // });
+
+  // console.log(nuxtApp)
 
   // return {
   //   provide: {
