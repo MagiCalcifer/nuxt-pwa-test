e2afada8751a66eb9a81c94b3c4931ea88c62710
Magical Calcifer <magicalcifer@protonmail.com>
2024-01-23 20:43:10 +0800

    one sig?

--- app.vue
+++ app.vue
@@ -562,17 +562,18 @@ const attemptReconnect = async () => {
 
 onMounted(() => {
 
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(function(OneSignal) {
-    OneSignal.init({
+  // window.OneSignalDeferred = window.OneSignalDeferred || [];
+  // OneSignalDeferred.push(function(OneSignal) {
+    
+  // });
+
+  OneSignal.init({
       appId: "128191f3-21e0-40b9-9567-4d7c0cc0e387",
       safari_web_id: "web.onesignal.auto.27be598e-7a22-4ed6-a01a-10378439b214",
       notifyButton: {
         enable: true,
       },
     });
-  });
-
   attemptReconnect()
 
 
